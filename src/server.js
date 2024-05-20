1b8490060a8dad3427da544aba57ddcee3c0e886
Put_tisan <109417391+Nagamiuko@users.noreply.github.com>
2024-05-20 14:26:53 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 // export const server = "https://manga-api-qt7v.onrender.com"
-export const server = "http://localhost:4002"
-export const urlserverview = "http://localhost:3000"
+export const server = "https://manga-api-qt7v.onrender.com"
+export const urlserverview = "https://project-final-eight.vercel.app/"
